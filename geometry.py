9d5cdd1ae41336cac107ba75b09bc80a1bf2f819
TkskKurumi <zafkielkurumi@gmail.com>
2021-09-03 09:38:29 +0800

    add geometry algorithms

---
+++ geometry.py
@@ -0,0 +1,59 @@
+def asign(i,eps=1e-8):
+    if(i>eps):
+        return 1
+    elif(i<-eps):
+        return -1
+    else:
+        return 0
+def aequal(x,y,eps=1e-8):
+    return asign(x-y)==0
+class point3d:
+    def __init__(self,x,y,z):
+        self.x=x
+        self.y=y
+        self.z=z
+    def __iter__(self):
+        return tuple([self.x,self.y,self.z]).__iter__()
+    def __pow__(self,other):
+        if(isinstance(other,point3d)):
+            #| i  j k | 
+            #|x1 y1 z1|=|y1 z1| * i + |z1 x1| * j + |x1 y1| * k
+            #|x2 y2 z2| |y2 z2|       |z2 x2|       |x2 y2|
+            x1,y1,z1=tuple(self)
+            x2,y2,z2=tuple(other)
+            x=y1*z2-z1*y2
+            y=z1*x2-x1*z2
+            z=x1*y2-y1*x2
+            return point3d(x,y,z)
+        else:
+            return NotImplemented
+    def __str__(self):
+        tup=tuple(self)
+        return 'point(%s)'%(tup,)
+    def __equal__(self,other):
+        if(isinstance(other,point3d)):
+            x1,y1,z1=tuple(self)
+            x2,y2,z2=tuple(other)
+            return aequal(x1,x2) and aequal(y1,y2) and aequal(z1,z2)
+        else:
+            return NotImplemented
+    def __neg__(self):
+        return point3d(-self.x,-self.y,-self.z)
+    def __add__(self,other):
+        if(isinstance(other,point3d)):
+            x1,y1,z1=tuple(self)
+            x2,y2,z2=tuple(other)
+            return point3d(x1+x2,y1+y2,z1+z2)
+        else:
+            return NotImplemented
+    def __sub__(self,other):
+        if(isinstance(other,point3d)):
+            x1,y1,z1=tuple(self)
+            x2,y2,z2=tuple(other)
+            return point3d(x1-x2,y1-y2,z1-z2)
+        else:
+            return NotImplemented
+if(__name__=='__main__'):
+    pos_x=point3d(1,0,0)
+    pos_y=point3d(0,1,0)
+    print(pos_x**pos_y)
